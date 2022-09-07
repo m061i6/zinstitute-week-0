9732aaecb23e20eb286b05bcf1872e4a521b19f1
m061i6 <oo00oo910@gmail.com>
2022-09-08 01:15:47 +0800

    fix error

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -12,8 +12,7 @@ type CartItemProps = {
 export function CartItem ( {id, quantity} : CartItemProps) {
     const { removeFromCart } = useShoppingCart()
     const item = storeItems.find(i => i.id === id)
-    if(item === null) return null
-
+    if(item == null) return null
     return(
         <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
             <img src={item.imgUrl} style={{ width: "125px", height:"75px", objectFit:"cover"}}/>
